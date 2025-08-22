1391d9f38918a8004921196d67ee03d0eae165e2
Yuri Serka <yserka@hotmail.com>
2025-08-22 13:29:35 -0300

    feat: fill board with all pieces

--- src/Game/Board.ts
+++ src/Game/Board.ts
@@ -1,6 +1,10 @@
+import { Bishop } from "./Pieces/Bishop";
+import { King } from "./Pieces/King";
+import { Knight } from "./Pieces/Knight";
 import type { Position } from "./Pieces/Move";
 import { Pawn } from "./Pieces/Pawn";
 import type { Piece } from "./Pieces/Piece";
+import { Queen } from "./Pieces/Queen";
 import { Rook } from "./Pieces/Rook";
 
 export class Board {
@@ -50,8 +54,8 @@ export class Board {
 
   private fillBoard() {
     for (let i = 0; i < 8; i++) {
-      const whitePawn = new Pawn("white", { x: 1, y: i });
-      const blackPawn = new Pawn("black", { x: 6, y: i });
+      const whitePawn = new Pawn("white", { x: 6, y: i });
+      const blackPawn = new Pawn("black", { x: 1, y: i });
 
       this.setPiece(whitePawn.position, whitePawn);
       this.setPiece(blackPawn.position, blackPawn);
@@ -59,14 +63,39 @@ export class Board {
 
     for (let i = 0, j = 7; ; ) {
       [
-        new Rook("white", { x: 0, y: i }),
-        new Rook("white", { x: 0, y: j }),
-        new Rook("black", { x: 7, y: i }),
-        new Rook("black", { x: 7, y: j }),
-      ].forEach((rook) => {
-        this.setPiece(rook.position, rook);
-      });
+        new Rook("white", { x: 7, y: i }),
+        new Rook("white", { x: 7, y: j }),
+        new Rook("black", { x: 0, y: i }),
+        new Rook("black", { x: 0, y: j }),
+      ].forEach((rook) => this.setPiece(rook.position, rook));
       i++, j--;
+
+      [
+        new Knight("white", { x: 7, y: i }),
+        new Knight("white", { x: 7, y: j }),
+        new Knight("black", { x: 0, y: i }),
+        new Knight("black", { x: 0, y: j }),
+      ].forEach((knight) => this.setPiece(knight.position, knight));
+      i++, j--;
+
+      [
+        new Bishop("white", { x: 7, y: i }),
+        new Bishop("white", { x: 7, y: j }),
+        new Bishop("black", { x: 0, y: i }),
+        new Bishop("black", { x: 0, y: j }),
+      ].forEach((bishop) => this.setPiece(bishop.position, bishop));
+      i++, j--;
+
+      [
+        new Queen("white", { x: 7, y: i }),
+        new Queen("black", { x: 0, y: i }),
+      ].forEach((queen) => this.setPiece(queen.position, queen));
+      i++, j--;
+
+      [
+        new King("white", { x: 7, y: i }),
+        new King("black", { x: 0, y: i }),
+      ].forEach((king) => this.setPiece(king.position, king));
 
       break;
     }
