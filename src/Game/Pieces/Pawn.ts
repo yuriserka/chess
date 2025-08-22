d16c572bd11e64f437a5b24c5ce34ebcc891cdd4
Yuri Serka <yserka@hotmail.com>
2025-08-22 13:29:57 -0300

    refac: pawn now is compliance with new interface

--- src/Game/Pieces/Pawn.ts
+++ src/Game/Pieces/Pawn.ts
@@ -1,93 +1,71 @@
-import { Board } from "../Board";
 import { Move, type Position } from "./Move";
-import { Piece, type PieceColor } from "./Piece";
+import { Piece, type Direction, type PieceColor } from "./Piece";
+import { Rook } from "./Rook";
 
 export class Pawn extends Piece {
   constructor(color: PieceColor, position: Position) {
     super(color, "pawn", position);
   }
 
-  getMoves(board: Board): Move[] {
+  getDirections(): Direction[] {
+    const direction = this.color === "black" ? 1 : -1;
+
+    const isInitialPosition =
+      (this.position.x === 6 && this.color === "white") ||
+      (this.position.x === 1 && this.color === "black");
+
     return [
-      this.getForwardMove(board),
-      this.getDoubleForwardMove(board),
-      ...this.getCaptureMove(board),
-    ].filter((move) => move !== null);
+      {
+        x: direction,
+        y: 0,
+        condition: (_, pieceAtNewPosition) => !pieceAtNewPosition,
+      }, // forward
+      {
+        x: 2 * direction,
+        y: 0,
+        condition: (board, pieceAtNewPosition) =>
+          isInitialPosition &&
+          !pieceAtNewPosition &&
+          !board.getPiece({
+            x: this.position.x + direction,
+            y: this.position.y,
+          }),
+      }, // double forward
+      {
+        x: direction,
+        y: 1,
+        condition: (_, pieceAtNewPosition) =>
+          !!pieceAtNewPosition && pieceAtNewPosition?.color !== this.color,
+      }, // capture right
+      {
+        x: direction,
+        y: -1,
+        condition: (_, pieceAtNewPosition) =>
+          !!pieceAtNewPosition && pieceAtNewPosition?.color !== this.color,
+      }, // capture left
+    ];
   }
 
-  private getDirection(): number {
-    return this.color === "white" ? 1 : -1;
-  }
+  getMovesInternal(
+    _: Piece | null,
+    newPosition: Position,
+    moves: Move[]
+  ): { shouldStop: boolean } {
+    const isFinalPosition =
+      (newPosition.x === 0 && this.color === "white") ||
+      (newPosition.x === 7 && this.color === "black");
 
-  private canMoveForward(board: Board, direction: number) {
-    const forward = this.position.x + direction;
-    const newPosition = { x: forward, y: this.position.y };
-    return {
-      canMove:
-        board.isValidPosition(newPosition) &&
-        board.getPiece(newPosition) === null,
-      newPosition: newPosition,
-    };
-  }
-
-  private getForwardMove(board: Board): Move | null {
-    const direction = this.getDirection();
-    const { canMove, newPosition } = this.canMoveForward(board, direction);
-    if (canMove) {
-      return new Move(this.position, newPosition);
-    }
-    return null;
-  }
-
-  private getDoubleForwardMove(board: Board): Move | null {
-    if (!this.getForwardMove(board)) {
-      return null;
-    }
-
-    const isWhiteInitial = this.position.x === 1 && this.color === "white";
-    const isBlackInitial = this.position.x === 6 && this.color === "black";
-
-    if (isWhiteInitial || isBlackInitial) {
-      const direction = this.getDirection();
-      const { canMove, newPosition } = this.canMoveForward(
-        board,
-        2 * direction
+    if (!isFinalPosition) {
+      moves.push(new Move(this.position, newPosition));
+    } else {
+      moves.push(
+        new Move(this.position, newPosition, (board) => {
+          // TODO: handle promotion
+          board.setPiece(newPosition, new Rook(this.color, newPosition));
+        })
       );
-      if (canMove) {
-        return new Move(this.position, newPosition);
-      }
     }
-    return null;
-  }
 
-  private canMoveDiagonally(board: Board, direction: number) {
-    const forward = this.position.x + direction;
-    const newPositionRight = { x: forward, y: this.position.y - direction };
-    const newPositionLeft = { x: forward, y: this.position.y + direction };
-    return {
-      canMoveRight:
-        board.isValidPosition(newPositionRight) &&
-        board.getPiece(newPositionRight) !== null,
-      newPositionRight: newPositionRight,
-      canMoveLeft:
-        board.isValidPosition(newPositionLeft) &&
-        board.getPiece(newPositionLeft) !== null,
-      newPositionLeft: newPositionLeft,
-    };
-  }
-
-  private getCaptureMove(board: Board): Move[] {
-    const direction = this.getDirection();
-    const { canMoveRight, newPositionRight, canMoveLeft, newPositionLeft } =
-      this.canMoveDiagonally(board, direction);
-
-    let moves: Move[] = [];
-    if (canMoveRight) {
-      moves.push(new Move(this.position, newPositionRight));
-    }
-    if (canMoveLeft) {
-      moves.push(new Move(this.position, newPositionLeft));
-    }
-    return moves;
+    return { shouldStop: true };
   }
 }
