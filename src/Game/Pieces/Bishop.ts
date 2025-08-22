b9ba5d498e5f6af0292185efd1207bd5eb3364b3
Yuri Serka <yserka@hotmail.com>
2025-08-22 13:28:27 -0300

    feat: add bishop

---
+++ src/Game/Pieces/Bishop.ts
@@ -0,0 +1,33 @@
+import { Move, type Position } from "./Move";
+import { Piece, type PieceColor } from "./Piece";
+
+export class Bishop extends Piece {
+  constructor(color: PieceColor, position: Position) {
+    super(color, "bishop", position, "B");
+  }
+
+  getDirections(): Position[] {
+    return [
+      { x: 1, y: 1 }, // down-right
+      { x: 1, y: -1 }, // down-left
+      { x: -1, y: 1 }, // up-right
+      { x: -1, y: -1 }, // up-left
+    ];
+  }
+
+  getMovesInternal(
+    pieceAtNewPosition: Piece | null,
+    newPosition: Position,
+    moves: Move[]
+  ): { shouldStop: boolean } {
+    if (!pieceAtNewPosition) {
+      moves.push(new Move(this.position, newPosition));
+    } else {
+      if (pieceAtNewPosition.color !== this.color) {
+        moves.push(new Move(this.position, newPosition));
+      }
+      return { shouldStop: true };
+    }
+    return { shouldStop: false };
+  }
+}
